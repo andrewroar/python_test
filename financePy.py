3dd3cf24f8a2bd1290bfd9728ff7cda5a356acec
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-10-11 15:41:24 +0100

    10/10/2020 update

--- financePy.py
+++ financePy.py
@@ -26,16 +26,33 @@ PG = wb.DataReader("PG", data_source = "yahoo", start="2010-1-1", end=today)
 ##########
 
 PG["simple return"]=(PG["Adj Close"]/PG["Adj Close"].shift(1))-1
-print(PG["simple return"])
+#print(PG["simple return"])
 PG["simple return"].to_csv("PG_Simple.csv")
 PG["simple return"].plot(figsize=(8,5))
 
+#################
+PG["log return"]=np.log((PG["Adj Close"]/PG["Adj Close"].shift(1)))
+#PG["log return"].plot(figsize=(8,5))
+#plt.show()
+#log return by year
+#log_return_a = PG["log return"].mean()*250
 
-#ABC={}
-#with open("test.csv") as f:
-#    lis = [line.split(",") for line in f]
-#    for i, x in enumerate(lis):
-#
-#      if x[1] == "ABC":
-#        print(1)
-#print(type(ABC))
+
+#####################
+Company_array = ["PG","MSFT","F","GE"]
+for t in Company_array:
+    new_data[t] = wb.DataReader(t, data_source = "yahoo", start="2000-1-1", end="2020-1-10")["Close"]
+print(new_data)
+#get first row
+print(new_data/new_data.iloc[0]*100)
+returns = (new_data/new_data.shift(-1))-1
+annual_returns = returns.mean()*250
+weights = np.array([0.25,0.25,0.25,0.25])
+print(returns)
+np.dot(annual_returns,weights)
+print(np.dot(annual_returns,weights))
+
+#(new_data/new_data.iloc[0]*100).plot(figsize=(8,5))
+#((new_data["PG"]/new_data["PG"].shift(-1))-1).plot(figsize=(8,5))
+
+plt.show()
